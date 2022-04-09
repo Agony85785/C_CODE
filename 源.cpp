45db8731109cbba9665a72c707c79fceeb727e56
Agony85785 <1910855115@qq.com>
2022-04-09 19:49:40 +0800

    矩阵连乘

--- 源.cpp
+++ 源.cpp
@@ -1,50 +1,66 @@
 #include<iostream>
 using namespace std;
-int flag = true,i=1;
-int BSearch(int a[],int x,int m,int n)
+void MatrixChain(int *p,int n,int**m,int **s)
 {
-	int left = m, right = n;
-	if(left>=right&&x!=a[left])
+	for (int i = 1; i <= n; i++)
+		m[i][i] = 0;
+    for(int r=2;r<=n;r++)
 	{
-		cout << "抱歉，没有这个数" << endl;
-		return 0;
-	}
-	if (flag = true)
-	{
-		flag = false;
-		if (x == a[0])
+		for (int i = 1;i<=n-r+1;i++)
 		{
-			cout << "找到了,该数在数组中的下标为：0" << endl;
-			return 0;
+			int j = i + r - 1;
+			m[i][j] = m[i + 1][j] + p[i - 1] * p[i] * p[j];
+			s[i][j] = i;
+			for(int k=i+1;k<j;k++)
+			{
+				int t = m[i][k]+m[k+1][j] + p[i - 1] * p[k] * p[j];
+				if(t<m[i][j])
+				{
+					m[i][j] = t;
+					s[i][j] = k;
+				}
+			}
+			
 		}
 	}
-	while(left<=right)
-	{
-		int middle = (left + right) / 2;
-		if (x == a[middle])
-			return middle;
-		if (x > a[middle])
-		{
-			cout << "子问题划分为[" << i << "]:在数组arr[" << middle + 1 << "]-arr[" << right << "]中继续查找" << endl; i++;
-			return BSearch(a, x, middle + 1, right);
-		}
-		if (x < a[middle])
-		{
-			cout << "子问题划分为[" << i << "]:在数组arr[" << left << "]-arr[" << middle - 1 << "]中继续查找" << endl; i++;
-			return BSearch(a, x, left, middle - 1);
-		}
-	}
+	
 }
 
+void Traceback(int i, int j,int **s)
+{
+	if (i == j)
+	{
+		cout << "A[" << i << "]";
+		return;
+	}
+	cout << "(";
+	Traceback(i, s[i][j], s);
+	Traceback(s[i][j]+1, j, s);
+	cout << ")";
+}
 int main()
 {
-	int a, b, res, arr[200];
-	for (a = 0; a < 200; a++)
-		arr[a] = a;
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	cout << "请输入你要查找的数：" << endl;
-	cin >> b;
-	res = BSearch(arr, b, 0, sz - 1);
-	if (res != 0)
-		cout << "找到了,该数在数组中的下标为："<<res << endl;
+	int p[11];
+	void Traceback(int i, int j,int** s);
+	void MatrixChain(int* p, int n, int** m, int** s);
+	int**m=new int* [11],**s=new int*[11];
+	for (int i = 0; i < 11; i++)
+	{
+		m[i] = new int[11];
+		s[i] = new int[11];
+	}
+	for (int i = 0; i <= 10; i++)
+		p[i] = i + 1;
+	MatrixChain(p,10,m ,s);
+	cout << "该A[1:10]矩阵连乘的最优计算次序为：";
+	Traceback(1, 10, s);
+	//for (int i = 1; i <= 10; i++)
+	//{
+	//	for (int j = 1; j <= 10; j++)
+	//	{if(i<=j)
+	//		cout << "m["<<i<<"]["<<j<<"]:"<<m[i][j] <<"   ";
+	//	}
+	//	cout << endl;
+	//}
+	cout << "\n最小计算量的值为：" << m[1][10];
 }
